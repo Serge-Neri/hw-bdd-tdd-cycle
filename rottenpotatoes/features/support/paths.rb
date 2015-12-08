cb2cde6fcb4bfa0ca060c4da88989e65b9301616
serge_neri <saneri@uncc.edu>
2015-12-08 19:43:57 +0000

    after fixing path

--- rottenpotatoes/features/support/paths.rb
+++ rottenpotatoes/features/support/paths.rb
@@ -13,15 +13,17 @@ module NavigationHelpers
   def path_to(page_name)
     case page_name
 
-    when /^the home\s?page$/
-      '/'
-
-    # Add more mappings here.
-    # Here is an example that pulls values out of the Regexp:
-    #
-    #   when /^(.*)'s profile page$/i
-    #     user_profile_path(User.find_by_login($1))
-
+    when /the home\s?page/
+      '/movies'
+    when /^the edit page for "([^"]+)"$/
+      edit_movie_path(Movie.find_by_title($1).id)
+    when /^the details page for "([^"]+)"$/
+      movie_path(Movie.find_by_title($1).id)
+    when /^the Similar Movies page for "([^"]+)"$/
+      @movie_id = Movie.find_by_title($1).id
+      movie_similar_director_path(@movie_id)
+    when /^the RottenPotatoes home page$/
+      movies_path
     else
       begin
         page_name =~ /^the (.*) page$/
