41219d661cb7499b451178526e010b17a37e2f9a
serge_neri <saneri@uncc.edu>
2015-12-09 18:54:57 +0000

    passing.

--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -1,5 +1,7 @@
 Rottenpotatoes::Application.routes.draw do
-  resources :movies
+  resources :movies do
+     get 'similar_director', to: 'movies#similar_director'
+  end
   # map '/' to be a redirect to '/movies'
   root :to => redirect('/movies')
 end
